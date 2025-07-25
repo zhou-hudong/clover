20077fff6f73d65112a30e981fb1ecac3b5cd178
FengLing X <zhouhudong666@gmail.com>
2025-07-25 15:04:20 +0200

    load create admin file

---
+++ create_admin.py
@@ -0,0 +1,21 @@
+
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = 'admin'
+email = 'zhouhudong666@gmail.com'
+password = '182692'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
