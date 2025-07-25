bf634b4a6804b64da2f770dddca24be3d6d0eb41
FengLing X <zhouhudong666@gmail.com>
2025-07-25 14:49:43 +0200

    update setting for migrate

--- barbershop_project/settings.py
+++ barbershop_project/settings.py
@@ -87,13 +87,17 @@ WSGI_APPLICATION = 'barbershop_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
